1eddb11b866af2b8c2d393f672d46b2284d17b91
gabrielgatu <gabriel.dny@gmail.com>
2015-02-16 12:22:15 +0100

    Add new ability to question and migrate from haml to erb

--- Gemfile
+++ Gemfile
@@ -10,6 +10,5 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
-gem 'haml', '~> 4.0.6'
 gem 'simple_form', '~> 3.1.0'
 gem 'bootstrap-sass', '~> 3.3.3'
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+	before_action :find_question, only: [:show, :edit, :update, :destroy]
 
 	def index
 	end
@@ -7,9 +8,17 @@ class QuestionsController < ApplicationController
 	end
 
 	def new
+		@question = Question.new
 	end
 
 	def create
+		@question = Question.new(question_params)
+
+		if @question.save
+			redirect_to root_path
+		else
+			render "new"
+		end
 	end
 
 	def edit
@@ -20,5 +29,15 @@ class QuestionsController < ApplicationController
 
 	def destroy
 	end 
+
+	private 
+
+	def question_params
+		params.require(:question).permit(:title, :content)
+	end
+
+	def find_question
+		@question = Question.find(params[:id])
+	end
 
 end
